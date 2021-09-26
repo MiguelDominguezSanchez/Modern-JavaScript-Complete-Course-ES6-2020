74af2a81c8745ba3f9c651b3906d2f3166cfb649
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-26 11:19:38 +0200

    // - 3 JavaScript and HTML -

--- modern_javascript/script.js
+++ modern_javascript/script.js
@@ -7,3 +7,14 @@ alert('Hi Laurence Svekis')
 */
 
 // - 3 JavaScript and HTML -
+
+// alert('hello')
+// Single comment
+alert('test')
+alert('test1') // second popup window
+/*
+Multi Line
+Comment
+
+
+*/
