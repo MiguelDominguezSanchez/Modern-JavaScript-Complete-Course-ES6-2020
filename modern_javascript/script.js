9e815f03a462bd2220931776a31b3faaadca3048
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-27 16:45:01 +0200

    // - 32 Number Guesser Challenge -

--- modern_javascript/script.js
+++ modern_javascript/script.js
@@ -1011,3 +1011,28 @@ function loop1(val) {
 
 loop1(5)
 */
+
+// - 32 Number Guesser Challenge -
+
+/*
+let secretNum = 5
+let message
+output(guesser('Guess a number 1-10'))
+
+function guesser(p) {
+	let guess = prompt(p)
+	guess = Number(guess)
+	if (guess == secretNum) {
+		message = `You got it ${secretNum}`
+		return message
+	} else {
+		message =
+			guess > secretNum ? `Was higher guess again` : `Was lower guess again`
+		return guesser(`[${guess}] ${message}`)
+	}
+}
+
+function output(mes) {
+	document.getElementById('message').innerHTML = mes
+}
+*/
