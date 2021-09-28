1149f8fdad85f371526013bbeb93fae1cb6b33de
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-28 12:19:33 +0200

    // - 3 Element Selectors -

--- modern_javascript/script.js
+++ modern_javascript/script.js
@@ -1640,3 +1640,22 @@ const mainH1 = document.getElementById('message')
 mainH1.style.backgroundColor = 'blue'
 mainH1.innerHTML = 'Hello World'
 */
+
+// - 3 Element Selectors -
+
+/*
+const message = document.getElementById('message')
+console.log(message)
+const listItems = document.getElementsByTagName('li')
+console.log(listItems)
+const highlights = document.getElementsByClassName('highlights')
+console.log(highlights)
+// console.log((highlights[0].textContent = 'TWO'))
+// console.log(listItem[0].textContent)
+const val1 = document.querySelector('li')
+console.log(val1)
+const val2 = document.querySelector('#message')
+console.log(val2)
+const val3 = document.querySelector('.highlight')
+console.log(val3)
+*/
